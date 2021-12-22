1bf7f793f5f7a4f7af4d7adb12f61154d0e6e30b
OM SAI <OM SAI@DESKTOP-28KFATM>
2021-12-22 10:50:22 +0530

    Even program is written by TL

---
+++ DemoProj1/src/com/nt/beans/EvenNo.java
@@ -0,0 +1,16 @@
+package com.nt.beans;
+
+public class EvenNo 
+{
+	public static void main(String[] args)
+	{
+		//Even number from 1 to 10
+		for(int i=1;i<=10;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+}
